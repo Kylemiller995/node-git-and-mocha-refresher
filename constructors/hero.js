c986bc7483785fd0dd89ee3f90a9e8dc302a718c
Kyle Miller <kylemiller995@gmail.com>
2017-12-27 17:02:27 +0000

    add hero extension

--- constructors/hero.js
+++ constructors/hero.js
@@ -9,5 +9,20 @@ Hero.prototype.talk = function () {
   return "I'm here to help!"
 };
 
+Hero.prototype.eat = function (food) {
+  var healingValue = food.healingValue
+  if(this.favouriteFood === food.name){
+    healingValue = healingValue * 2
+  }
+  if(food.poisoned === true){
+    healingValue = healingValue * -1
+  }
+  if(this.health + healingValue > 100){
+    this.health = 100
+  } else {
+    this.health += healingValue
+  }
+}
+
 
 module.exports = Hero;
--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -1,12 +1,15 @@
 var assert = require('assert');
 var Hero = require('../constructors/hero.js');
+var Food = require('../constructors/food.js');
 
 describe('Hero', function () {
 
 	var hero;
 
 	beforeEach(function () {
-		hero = new Hero( "bob", "pizza" )
+		hero = new Hero( "bob", "human" )
+    pizza = new Food( "pizza", 20 )
+    human = new Food( "human", 5 )
 	})
 
 	it('should have a name', function () {
@@ -15,6 +18,30 @@ describe('Hero', function () {
 
   it('should be able to talk', function(){
     assert.strictEqual(hero.talk(), 'I\'m here to help!')
+  })
+
+  it('should only heal to 100 if healing amount + hp exceeds 100', function(){
+    hero.health = 89
+    hero.eat(pizza)
+    assert.strictEqual(hero.health, 100)
+  })
+
+  it('should heal the correct amount', function() {
+    hero.health = 30
+    hero.eat(pizza)
+    assert.strictEqual(hero.health, 50)
+  })
+
+  it('should damage if poisoned', function() {
+    pizza.poison()
+    hero.eat(pizza)
+    assert.strictEqual(hero.health, 80)
+  })
+
+  it('should increase by more if fav food', function() {
+    hero.health = 10
+    hero.eat(human)
+    assert.strictEqual(hero.health, 20)
   })
 
 })
